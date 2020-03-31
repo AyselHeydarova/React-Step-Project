1efa615d7d3c6fe4f09fd27ceb3f2116220b0381
Aysel <aysel@MacBook-Pro-Aysel.local>
2020-03-31 19:49:10 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+class App extends Component {
+
+    render() {
+
+        return (
+            <>
+                <h1>React Step Project</h1>
+            </>
+        );
+    }
+}
+export default App;

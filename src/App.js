d5595a1516dd9d72f6f712fe108a4f70a58ba61d
Aysel <aysel@MacBook-Pro-Aysel.local>
2020-03-31 21:05:28 +0400

    created show Alert function

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import React, {Component} from 'react';
 
 class App extends Component {
 
-    render() {
 
+
+    render() {
+        function showAlert() {
+            alert('Welcome to Step Project!');
+        }
+
+        showAlert();
         return (
             <>
                 <h1>React Step Project</h1>
+
+                <p>Note items will be shown here</p>
+
+
             </>
         );
     }

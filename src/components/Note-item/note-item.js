48abc0ada8e5fec71be4b667f672784182e26f67
AysekHeydarova <aysel.esedova1994@gmail.com>
2020-04-03 02:53:51 +0400

    Single note opens onclick

--- src/components/Note-item/note-item.js
+++ src/components/Note-item/note-item.js
@@ -1,61 +1,70 @@
 import React from 'react';
-import './note-item-style.scss'
+import './note-item-style.scss';
+import {Link} from "react-router-dom";
+import {Route} from 'react-router-dom';
+import SingleNote from "../SingleNote";
+
+
 const NoteItem = props => {
-    const colors=[
-         {
-             "bgc":"rgb(255, 204, 204)",
-             "border": "rgb(255, 102, 102)"
-         },
-         {
-             "bgc":"rgb(179, 230, 204)",
-             "border": "rgb(64, 191, 125)"
-         },
+    const colors = [
         {
-            "bgc":"rgb(153, 221, 255)",
+            "bgc": "rgb(255, 204, 204)",
+            "border": "rgb(255, 102, 102)"
+        },
+        {
+            "bgc": "rgb(179, 230, 204)",
+            "border": "rgb(64, 191, 125)"
+        },
+        {
+            "bgc": "rgb(153, 221, 255)",
             "border": "rgb(0, 170, 255)"
         },
         {
-            "bgc":"rgb(255, 255, 204)",
+            "bgc": "rgb(255, 255, 204)",
             "border": "rgb(255, 255, 0)"
         }
 
     ];
-    let color={};
-    let defineColor=()=>{
-      switch (props.color) {
-          case "red":{
-              color=colors[0];
-              break;
-          }
-          case "green":{
-              color=colors[1];
-              break;
-          }
-          case "blue":{
-              color=colors[2];
-              break;
-          }
-          case "yellow":{
-              color=colors[3];
-              break;
-          }
-          default:{
-              color=colors[Math.random()*4];
-              break;
-          }
-      }
+    let color = {};
+    let defineColor = () => {
+        switch (props.color) {
+            case "red": {
+                color = colors[0];
+                break;
+            }
+            case "green": {
+                color = colors[1];
+                break;
+            }
+            case "blue": {
+                color = colors[2];
+                break;
+            }
+            case "yellow": {
+                color = colors[3];
+                break;
+            }
+            default: {
+                color = colors[Math.random() * 4];
+                break;
+            }
+        }
     };
+
     defineColor();
 
+
     return (
-        <div className={"note-item"} style={{backgroundColor:color.bgc, border: `1px solid ${color.border}`}}>
-            <div style={{borderBottom:`1px solid ${color.border}`}}>
-                <h3>{props.title}</h3>
-            </div>
-            <div className={"note-item-title"}>
-                <p>{props.context}</p>
-            </div>
-        </div>
+
+                <div className={"note-item"} style={{backgroundColor: color.bgc, border: `1px solid ${color.border}`}}>
+                    <div style={{borderBottom: `1px solid ${color.border}`}}>
+                        <h3>{props.title}</h3>
+                    </div>
+                    <div className={"note-item-title"}>
+                        <p>{props.context}</p>
+                    </div>
+                </div>
+
     );
 };
 
---
+++ src/components/SingleNote.js
@@ -0,0 +1,30 @@
+import React, {useEffect, useState} from 'react';
+
+const SingleNote = ({match}) => {
+
+    const [note, setNote] = useState('');
+
+    useEffect( () => {
+        fetch(`http://localhost:3000/notes?id=${match.params.id}` || `http://localhost:3001/notes?id=${match.params.id}`)
+            .then(note=> note.json())
+            .then(note => setNote(note[0]))
+        }, []);
+
+
+    const {title, text, color} = note;
+
+    return (
+        <div>
+                <div>
+                    <h3>{title}</h3>
+                </div>
+
+                <div >
+                    <p>{text}</p>
+                </div>
+
+        </div>
+    );
+};
+
+export default SingleNote;

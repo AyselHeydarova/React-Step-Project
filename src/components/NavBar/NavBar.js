1645f165ef7a5455293390ee34d54d61865810cc
gunelaliyeva <gunel.aliyeva.3011@gmail.com>
2020-04-04 22:50:22 +0400

    logo issue fixed

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './NavBar.css';
 import {Link} from "react-router-dom";
-
+import logo from './Images/logo.png'
 
 const NavBar = () => {
     return (
         <header className={'header'}>
             <Link to={'/'} className={'app-info'}>
-                <img className={'logo'} src="./Images/logo.png"  alt="app logo"/>
+                <img className={'logo'} src={logo}  alt="app logo"/>
                 <h1 className={'app-name'}>NotesApp</h1>
             </Link>
 
